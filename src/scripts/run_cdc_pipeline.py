d3902298749ebda5f722ccc85a5d4205f59a3676
trucnmt <truc.ngmaithanh@gmail.com>
2024-06-10 00:42:50 +0700

    Update CDC Pipeline Scripts

--- src/scripts/run_cdc_pipeline.py
+++ src/scripts/run_cdc_pipeline.py
@@ -1,20 +1,8 @@
 from cdc.cdc_handler import CDCHandler
-
+from config.db_config import load_db_config
 def main():
-    config = {
-        'kafka_topic': "postgres.public.ingredients",
-        'kafka_bootstrap_servers': ["localhost:29092"],
-        'avro_schema_path': "./schema.avsc",
-        'snowflake_user': 'trucnmt',
-        'snowflake_password': 'Thanhtruc28!',
-        'snowflake_account': 'WK90181.ap-southeast-1',
-        'snowflake_warehouse': 'COMPUTE_WH',
-        'snowflake_database': 'POSTGRES',
-        'snowflake_schema': 'PUBLIC'
-    }
-
+    config = load_db_config('config/config.yaml')
     handler = CDCHandler(config)
     handler.process_changes()
-
 if __name__ == "__main__":
     main()
