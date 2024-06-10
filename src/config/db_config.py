dca54ba01eaa3f5d34a9c90e7a60dbf64282798b
trucnmt <truc.ngmaithanh@gmail.com>
2024-06-10 10:42:00 +0700

    Create configuration file for 3-Warehouse

--- src/config/db_config.py
+++ src/config/db_config.py
@@ -1,15 +1,17 @@
 import yaml
 
-def load_db_config(config_path='config/config.yaml'):
-    """
-    Load database configuration from a YAML file.
-    :param config_path: Path to the YAML configuration file
-    :return: Dictionary containing database configuration
-    """
-    with open(config_path, 'r') as file:
-        config = yaml.safe_load(file)
-    return config['database']
-
-# Example usage:
-# db_config = load_db_config()
-# print(db_config['postgres']['dbname'])
+class ConfigLoader:
+    @staticmethod
+    def load_db_config(file_path='configs/config.yaml'):
+        """
+        Load database configuration from a YAML file.
+        :param file_path: Path to the YAML configuration file
+        :return: Dictionary containing database configuration
+        """
+        with open(file_path, 'r') as file:
+            try:
+                config = yaml.safe_load(file)
+                return config['database']
+            except yaml.YAMLError as exc:
+                print(exc)
+                return None
