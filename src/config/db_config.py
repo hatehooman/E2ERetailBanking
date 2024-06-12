915e323035d3c63e9712d626fd14b714179d946c
trucnmt <truc.ngmaithanh@gmail.com>
2024-06-12 12:16:07 +0700

    Update configuration of DB

--- src/config/db_config.py
+++ src/config/db_config.py
@@ -2,16 +2,16 @@ import yaml
 
 class ConfigLoader:
     @staticmethod
-    def load_db_config(file_path='configs/config.yaml'):
+    def load_config(file_path='config/config.yaml'):
         """
-        Load database configuration from a YAML file.
+        Load configuration from a YAML file.
         :param file_path: Path to the YAML configuration file
-        :return: Dictionary containing database configuration
+        :return: Dictionary containing configuration
         """
         with open(file_path, 'r') as file:
             try:
                 config = yaml.safe_load(file)
                 return config['database']
             except yaml.YAMLError as exc:
-                print(exc)
+                print(f"Error loading configuration: {exc}")
                 return None
