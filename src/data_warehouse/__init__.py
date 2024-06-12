97c0936dccbe712a398b80ee626e7681d0497476
trucnmt <truc.ngmaithanh@gmail.com>
2024-06-12 11:47:23 +0700

    Update DW

---
+++ src/data_warehouse/__init__.py
@@ -0,0 +1,5 @@
+"""
+Data Warehouse Package
+----------------------
+This package contains modules for setting up, loading, transforming, and querying data in a Snowflake data warehouse.
+"""
--- src/data_warehouse/dw_transformer.py
+++ src/data_warehouse/dw_transformer.py
@@ -1,19 +1,70 @@
-class DWTransformer:
-    def __init__(self, config):
-        self.config = config
+import yaml
+from snowflake.connector import connect
 
-    def transform_data(self, data):
-        transformed_data = []
-        for record in data:
-            transformed_record = self._transform_record(record)
-            transformed_data.append(transformed_record)
+class DWTransformer:
+    def __init__(self, config_path):
+        self.config = self.load_config(config_path)
+        self.conn = self.connect_to_snowflake()
+
+    def load_config(self, config_path):
+        with open(config_path, 'r') as file:
+            config = yaml.safe_load(file)
+        return config
+
+    def connect_to_snowflake(self):
+        return connect(
+            user=self.config['snowflake']['user'],
+            password=self.config['snowflake']['password'],
+            account=self.config['snowflake']['account'],
+            warehouse=self.config['snowflake']['warehouse'],
+            database=self.config['snowflake']['database'],
+            schema=self.config['snowflake']['schema']
+        )
+
+    def transform_data(self, source_table, target_table, transformation_query):
+        # Load data from source_table
+        data = self.load_data(source_table)
+        
+        # Apply transformation
+        transformed_data = self.apply_transformation(data, transformation_query)
+        
+        # Save transformed data to target_table
+        self.save_data(transformed_data, target_table)
+
+    def load_data(self, source_table):
+        cursor = self.conn.cursor()
+        cursor.execute(f"SELECT * FROM {source_table}")
+        data = cursor.fetchall()
+        cursor.close()
+        return data
+
+    def apply_transformation(self, data, transformation_query):
+        cursor = self.conn.cursor()
+        cursor.execute(transformation_query)
+        transformed_data = cursor.fetchall()
+        cursor.close()
         return transformed_data
 
-    def _transform_record(self, record):
-        # Implement transformation logic here
-        return record
+    def save_data(self, transformed_data, target_table):
+        cursor = self.conn.cursor()
+        # Assume transformed_data is a list of tuples matching the target_table schema
+        insert_query = f"INSERT INTO {target_table} VALUES " + ",".join(
+            str(tuple(record)) for record in transformed_data
+        )
+        cursor.execute(insert_query)
+        cursor.close()
+
+    def close_connection(self):
+        self.conn.close()
 
 # Example usage:
-# config = load_config('config/config.yaml')
-# transformer = DWTransformer(config)
-# transformed_data = transformer.transform_data(data)
+# config_path = 'config/config.yaml'
+# source_table = 'STAGING.RAW.raw_table'
+# target_table = 'STAGING.CLEAN.cleaned_table'
+# transformation_query = '''
+#     SELECT col1, col2, col3 * 2 as col3
+#     FROM STAGING.RAW.raw_table
+# '''
+# transformer = DWTransformer(config_path)
+# transformer.transform_data(source_table, target_table, transformation_query)
+# transformer.close_connection()
